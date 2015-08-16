2f37a66a1b531eee7247d6c445a607dd33b37300
alexander <morozov_a@sibers.com>
2015-08-16 19:44:35 +0600

    add sample route and twig template

--- AppBundle/Controller/MightyMouseController.php
+++ AppBundle/Controller/MightyMouseController.php
@@ -2,9 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\DependencyInjection\ContainerAware;
 
 class MightyMouseController extends ContainerAware
 {
@@ -14,7 +14,7 @@ class MightyMouseController extends ContainerAware
     public function rescueAction()
     {
         $html = $this->container->get('twig')->render(
-            'mighty_mouse/rescue.html.twig',
+            'views/mighty_mouse/rescue.html.twig',
             array('quote' => 'Here I come to save the day!')
         );
 

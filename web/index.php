7a5c2bfba2b157f779a3e836026806f00c615406
alexander <morozov_a@sibers.com>
2015-08-16 18:08:21 +0600

    add front controller

---
+++ web/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+umask(0000);
+
+$env = 'dev';
+$debug = true;
+
+$loader = require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../AppKernel.php';
+
+if ($debug) {
+    Debug::enable();
+}
+$kernel = new AppKernel($env, $debug);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);

267771af2fe4c993c99b927f7a6c20a6a7d7e147
alexander <morozov_a@sibers.com>
2015-08-16 19:31:40 +0600

    add ContainerAware to use twig

--- AppBundle/Controller/MightyMouseController.php
+++ AppBundle/Controller/MightyMouseController.php
@@ -4,15 +4,21 @@ namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\DependencyInjection\ContainerAware;
 
-class MightyMouseController
+class MightyMouseController extends ContainerAware
 {
     /**
      * @Route("/")
      */
     public function rescueAction()
     {
-        return new Response('Here I come to save the day!');
+        $html = $this->container->get('twig')->render(
+            'mighty_mouse/rescue.html.twig',
+            array('quote' => 'Here I come to save the day!')
+        );
+
+        return new Response($html);
     }
 
 }
